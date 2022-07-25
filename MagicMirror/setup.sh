98e0e9bf399e58647bbbe930ff25d80dd8d98740
Master Pi <minh.vln140501@gmail.com>
2022-07-25 17:01:31 +0700

    Create setup.sh

--- MagicMirror/setup.sh
+++ MagicMirror/setup.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
-FILE="$(pwd)/setup.sh"
+
 # Make system new and shiny
 cd ~
 sudo apt update && sudo apt upgrade
-
-# Check if NodeJS is installed
-#if [! which node > /dev/null ]; then
-#	echo -e "NodeJS is not installed. Installing NodeJS v16.14.2"
-#	curl -sl https://deb.nodesource.com/setup_16.x | sudo -E bash
-#	sudo apt install -y nodejs gcc g++ make
-#	npm install -g pm2
-#	echo -e "NodeJS is installed!"
-#fi
 
 # Clone and build MagicMirror
 git clone https://github.com/MichMich/MagicMirror
@@ -19,7 +10,7 @@ cd MagicMirror
 npm install --only=prod --omit=dev
 
 # Add default config file
-wget -O config/config.js https://raw.githubusercontent.com/MasterPi-2124/SmartMirror/master/MagicMirror/config.js
+wget -O config/config.js https://raw.githubusercontent.com/MasterPi-2124/SmartMirror/master/config/config.js
 
 # Install Google Assistant
 cd modules
@@ -30,7 +21,7 @@ git clone https://github.com/bugsounet/EXT-Alert
 cd EXT-Detector && npm install
 cd ../Gateway && npm install
 cd ../EXT-Alert && npm install
-cd ../MMM-GoogleAssistant && wget https://raw.githubusercontent.com/MasterPi-2124/SmartMirror/master/MagicMirror/credentials.json && npm install
+cd ../MMM-GoogleAssistant && npm install
 
 
 
