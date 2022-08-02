2a2b9fe14945f6f65303d4a92b3a499506396651
Master Pi <minh.vln140501@gmail.com>
2022-08-02 23:33:49 +0700

    super dirty code

--- RaspiCaptureCam/python/helper.py
+++ RaspiCaptureCam/python/helper.py
@@ -1,4 +1,5 @@
 import cv2
+import os
 from gpiozero import MotionSensor
 
 pir = MotionSensor(27)
@@ -8,4 +9,5 @@ while True:
     pir.wait_for_motion()
     print("Motion detected!")
     cmd = 'raspistill -e png -o ~/Pictures/new_image.png -w 1280 -h 720 -vf -t 2000'
+    os.system(cmd)
     pir.wait_for_no_motion()
