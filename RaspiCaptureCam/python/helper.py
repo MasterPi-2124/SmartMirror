a9e4deea5a507b79a87873d1d9769f24567d5d93
Master Pi <minh.vln140501@gmail.com>
2022-08-02 23:40:10 +0700

    use cv2

--- RaspiCaptureCam/python/helper.py
+++ RaspiCaptureCam/python/helper.py
@@ -8,6 +8,15 @@ vid = cv2.VideoCapture(0)
 while True:
     pir.wait_for_motion()
     print("Motion detected!")
-    cmd = 'raspistill -e png -o ~/Pictures/new_image.png -w 1280 -h 720 -vf -t 2000'
-    os.system(cmd)
+    # cmd = 'raspistill -e png -o ~/Pictures/new_image.png -w 1280 -h 720 -vf -t 2000'
+    # os.system(cmd)
+    # pir.wait_for_no_motion()
+
+    while True:
+        ret, frame = vid.read()
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) $ 0xFF == ord('q'):
+            break
+    cv2.destroyAllWindows()
     pir.wait_for_no_motion()
+    vid.release()
