e09d73b354fcd08145b12636abf5079b832d3ce5
Master Pi <minh.vln140501@gmail.com>
2022-08-03 00:18:50 +0700

    merge code

--- GesturesDetection/predict.py
+++ GesturesDetection/predict.py
@@ -11,7 +11,11 @@ import tensorflow as tf
 import seaborn as sn
 import pandas as pd
 import random
-import time
+from time import sleep
+import cv2
+import os
+from time import perf_counter
+from gpiozero import MotionSensor
 from datetime import datetime
 
 
@@ -51,6 +55,22 @@ def predict():
     print(classes[np.argmax(y[0])])
     return
 
-while True:
-    predict()
-    time.sleep(5)
+if __name__ == "__main__":
+    pir = MotionSensor(27)
+    vid = cv2.VideoCapture(0)
+    print("File started!!!")
+    while True:
+        t1_start = perf_counter()
+        pir.wait_for_motion()
+        print("Motion detected! Sleep 3s before captured")
+        sleep(3)
+        ret, frame = vid.read()
+        cv2.imwrite('savedImage.png', frame)
+        print("image saved at savedImage.png. Processing...")
+        predict()
+        t1_stop = perf_counter()
+        print("Processed done. takes ", t1_stop - t1_start)
+        pir.wait_for_no_motion()
+        print("not detected motion.")
+
+    vid.release()
--- RaspiCaptureCam/python/helper.py
+++ RaspiCaptureCam/python/helper.py
@@ -1,25 +1,25 @@
+from time import sleep
 import cv2
 import os
+from time import perf_counter
 from gpiozero import MotionSensor
 
-pir = MotionSensor(27)
-vid = cv2.VideoCapture(0)
-
-while True:
-    pir.wait_for_motion()
-    print("Motion detected!")
-    # cmd = 'raspistill -e png -o ~/Pictures/new_image.png -w 1280 -h 720 -vf -t 2000'
-    # os.system(cmd)
-    # pir.wait_for_no_motion()
-
+if __name__ == "__main__":
+    pir = MotionSensor(27)
+    vid = cv2.VideoCapture(0)
+    print("File started!!!")
     while True:
+        t1_start = perf_counter()
+        pir.wait_for_motion()
+        print("Motion detected! Sleep 3s before captured")
+        sleep(3)
         ret, frame = vid.read()
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    filename = "save.png"
-    cv2.imwrite(filename, frame)
-    cv2.destroyAllWindows()
-    pir.wait_for_no_motion()
+        cv2.imwrite('savedImage.png', frame)
+        print("image saved at savedImage.png. Processing...")
+        predict()
+        t1_stop = perf_counter()
+        print("Processed done. takes ", t1_stop - t1_start)
+        pir.wait_for_no_motion()
+        print("not detected motion.")
 
-vid.release()
+    vid.release()
