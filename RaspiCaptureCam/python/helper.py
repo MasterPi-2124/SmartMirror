e2789a1404653199a3ed1b4ff9cf0b8e4db317ce
Master Pi <minh.vln140501@gmail.com>
2022-08-02 22:56:10 +0700

    oppsie

--- RaspiCaptureCam/python/helper.py
+++ RaspiCaptureCam/python/helper.py
@@ -1,27 +1,27 @@
-import RPi.GPIO as GPIO
-import time
 
-pir_sensor = 11
-piezo = 7
+# import the opencv library
+import cv2
 
-GPIO.setmode(GPIO.BOARD)
+from gpiozero import MotionSensor
 
-GPIO.setup(piezo,GPIO.OUT)
+# pir pin = 17
+pir = MotionSensor(17)
+# define a video capture object
+vid = cv2.VideoCapture(0)
 
-GPIO.setup(pir_sensor, GPIO.IN)
-
-current_state = 0
-try:
-    while True:
-        time.sleep(0.1)
-        current_state = GPIO.input(pir_sensor)
-        if current_state == 1:
-            print("GPIO pin %s is %s" % (pir_sensor, current_state))
-            GPIO.output(piezo,True)
-            time.sleep(1)
-            GPIO.output(piezo,False)
-            time.sleep(5)
-except KeyboardInterrupt:
-    pass
-finally:
-    GPIO.cleanup()
+while True:
+	pir.wait_for_motion()
+	print("Motion detected!")
+	ret, frame = vid.read()
+	# put ML model here
+	# frame is image
+ 	# Display the resulting frame
+    	cv2.imshow('frame', frame)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+	pir.wait_for_no_motion()
+  
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
