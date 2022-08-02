8041c20fe4161618599cfe0071f3dab90077dee2
Master Pi <minh.vln140501@gmail.com>
2022-08-03 00:55:42 +0700

    video release

--- GesturesDetection/predict.py
+++ GesturesDetection/predict.py
@@ -33,7 +33,7 @@ def build_model():
 
 working_directory = os.getcwd()
 print(working_directory)
-img_path = '{}/res/result.png'.format(working_directory)
+img_path = '{}/savedImage.png'.format(working_directory)
 model_link = '{}/result.h5'.format(working_directory)
 model = build_model()
 model.load_weights(model_link)
@@ -57,20 +57,19 @@ def predict():
 
 if __name__ == "__main__":
     pir = MotionSensor(27)
-    vid = cv2.VideoCapture(0)
     print("File started!!!")
     while True:
         pir.wait_for_motion()
         t1_start = perf_counter()
+        vid = cv2.VideoCapture(0)
         print("Motion detected! Sleep 3s before captured")
         sleep(3)
         ret, frame = vid.read()
         cv2.imwrite('savedImage.png', frame)
+        vid.release()
         print("image saved at savedImage.png. Processing...")
         predict()
         t1_stop = perf_counter()
         print("Processed done. takes ", t1_stop - t1_start)
         pir.wait_for_no_motion()
         print("not detected motion.")
-
-    vid.release()
