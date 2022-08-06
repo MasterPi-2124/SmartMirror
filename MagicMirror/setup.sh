24ffdcbc74583d991782198095d97d84ce608a34
Master Pi <minh.vln140501@gmail.com>
2022-08-06 16:40:57 +0700

    bash

--- MagicMirror/setup.sh
+++ MagicMirror/setup.sh
@@ -7,79 +7,138 @@ Blue='\033[0;34m'
 PURPLE='\033[0;35m'
 YELLOW='\033[1;33m'
 ORANGE='\033[0;33m'
-BOLD = '\033[1m'
+BOLD='\033[1m'
+NODE_TESTED="v16.9.1"
+NPM_TESTED="V7.11.2"
 
 # Make system new and shiny
-printf "${GREEN} Checking requirements ...\n${NC}"
+printf "${YELLOW}Checking requirements ...\n${NC}"
 
 raspi=$(raspi-config nonint get_pi_type)
 node_installed=$(which node)
 node_version=$($node_installed -v)
-npm_version=$(npm -v)
+npm_version='V'$(npm -v)
+arm=$(uname -m)
 
-if [ $raspi -ge 3 ]; then 
-    printf "${GREEN} Raspberry Pi $raspi detected!\n${NC}"
-else
-    printf "${RED} You are using uncompatible version of Pi. Please consider using pi3 or higher.\n${NC}"
-    exit 1
+function verlte() {  [ "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ];}
+function verlt() { [ "$1" = "$2" ] && return 1 || verlte $1 $2 ;}
+
+if [ 0 == 1 ]; then
+	if [ "$arm" != "armv7l" ]; then
+        printf "${RED}You are using uncompatible version of Pi. Please consider using pi3 or higher.\n${NC}"
+        exit 1
+	elif [ $raspi -ge 3 ]; then 
+        printf "${GREEN}Raspberry Pi $raspi detected!\n${NC}"
+    fi
 fi
 
 if [ -x "$(command -v node)" ]; then
-    printf "${GREEN} NodeJS is installed! Found version $node_version.\n${NC}"
+    if [ ${node_version:1:2} -gt ${NODE_TESTED:1:2} ]; then
+        printf "${GREEN}NodeJS is installed! Found version $node_version.\n${NC}"
+    else
+        printf "${RED}NodeJS version is too old! MagicMirror only run on NodeJS >= $NODE_TESTED.\n${NC}"
+        exit 1
+    fi
 else
-    printf "${RED} NodeJS is not installed! You must install NodeJS and npm to make it work.\n${NC}"
+    printf "${RED}NodeJS is not installed! You must install NodeJS and NPK to make it work.\n${NC}"
     exit 1
 fi
 
 if [ -x "$(command -v npm)" ]; then
-    printf "${GREEN} npm is installed! Found version $npm_version.\n${NC}"
+    if verlte $NPM_TESTED $npm_version; then
+        printf "${GREEN}NPM is installed! Found version $npm_version.\n${NC}"
+    else
+        printf "${RED}NPM version is too old! MagicMirror only run on NPM >= $NPM_TESTED.\n${NC}"
+        exit 1
+    fi
 else
-    printf "${RED} npm is not installed! You must install NodeJS and npm to make it work.\n${NC}"
+    printf "${RED}NPM is not installed! You must install NodeJS and NPK to make it work.\n${NC}"
     exit 1
 fi
 
-printf "${GREEN} Going to home directory...\n${NC}"
-printf "${GREEN} Updating the system...\n${NC}"
+printf "${YELLOW}Going to home directory...\n${NC}"
+printf "${YELLOW}Updating the system...\n${NC}"
 cd ~
 sudo apt update && sudo apt upgrade
 
-printf "${GREEN} System updated!\n${NC}"
+printf "${GREEN}System updated!\n${NC}"
 
 
-printf "${GREEN} Cloning MagicMirror...\n${NC}"
+printf "${YELLOW}Cloning MagicMirror...\n${NC}"
 # Clone and build MagicMirror
 git clone https://github.com/MichMich/MagicMirror
 cd MagicMirror
 npm install --only=prod --omit=dev
 
-printf "${GREEN} Getting default config... \n${NC}"
+install_status=$?
+if [ $install_status -eq 0 ]; then
+    printf "${GREEN}MagicMirror install completed!\n${NC}"
+else 
+    printf "${RED}MagicMirror install failed with errors!\n${NC}"
+    exit 1
+fi
+
+printf "${YELLOW}Getting default config... \n${NC}"
 # Add default config file
 wget -O config/config.js https://raw.githubusercontent.com/MasterPi-2124/SmartMirror/master/MagicMirror/config.js
 
 
 # Install Google Assistant
 cd modules
-printf "${GREEN} Cloning EXT-Detector... \n${NC}"
+printf "${YELLOW}Cloning EXT-Detector... \n${NC}"
 git clone https://github.com/bugsounet/EXT-Detector
 
-printf "${GREEN} Cloning MMM-GoogleAssistant... \n${NC}"
+printf "${YELLOW}Cloning MMM-GoogleAssistant... \n${NC}"
 git clone https://github.com/bugsounet/MMM-GoogleAssistant
 
-printf "${GREEN} Cloning Gateway... \n${NC}"
+printf "${YELLOW}Cloning Gateway... \n${NC}"
 git clone https://github.com/bugsounet/Gateway
 
-printf "${GREEN} Cloning EXT-Alert... \n${NC}"
+printf "${YELLOW}Cloning EXT-Alert... \n${NC}"
 git clone https://github.com/bugsounet/EXT-Alert
 
-printf "${GREEN} Cloning MMM-OpenCVGestures... \n${NC}"
+printf "${YELLOW}Cloning MMM-OpenCVGestures... \n${NC}"
 git clone https://github.com/MasterPi-2124/MMM-OpenCVGestures
 
 cd EXT-Detector && npm install
-cd ../Gateway && npm install
-cd ../EXT-Alert && npm install
-cd ../MMM-GoogleAssistant 
-wget https://raw.githubusercontent.com/MasterPi-2124/SmartMirror/master/MagicMirror/credentials.json && npm install
+install_status=$?
+if [ $install_status -eq 0 ]; then
+    printf "${GREEN}EXT-Detector install completed!\n${NC}"
+else 
+    printf "${RED}EXT-Detector install failed with errors!\n${NC}"
+    exit 1
+fi
 
-printf "${GREEN} Getting token for Google Assistant module..."
+cd ../Gateway && npm install
+install_status=$?
+if [ $install_status -eq 0 ]; then
+    printf "${GREEN}Gateway install completed!\n${NC}"
+else 
+    printf "${RED}Gateway install failed with errors!\n${NC}"
+    exit 1
+fi
+
+cd ../EXT-Alert && npm install
+install_status=$?
+if [ $install_status -eq 0 ]; then
+    printf "${GREEN}EXT-Alert install completed!\n${NC}"
+else 
+    printf "${RED}EXT-Alert install failed with errors!\n${NC}"
+    exit 1
+fi
+
+cd ../MMM-GoogleAssistant
+printf "${YELLOW}Installing credentials... \n${NC}"
+wget https://raw.githubusercontent.com/MasterPi-2124/SmartMirror/master/MagicMirror/credentials.json
+npm install
+install_status=$?
+if [ $install_status -eq 0 ]; then
+    printf "${GREEN}MMM-GoogleAssistant install completed!\n${NC}"
+else 
+    printf "${RED}MMM-GoogleAssistant install failed with errors!\n${NC}"
+    exit 1
+fi
+
+printf "${YELLOW}Getting token for Google Assistant module..."
 npm run token
-printf "${GREEN} MagicMirror is installed successfully! You can run it via \"npm run start\" command. \n${NC}"
+printf "\e[92mWe're ready! Run \e[1m\e[97m$rmessage\e[0m\e[92m from the ~/MagicMirror directory to start your MagicMirror.\e[0m"
